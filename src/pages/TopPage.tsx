7e41e87ee5a641a39e27e3bcbcedcb2516c2b546
reiji1020 <sk@reiji1020.info>
2022-08-31 01:36:51 +0900

    [update]タブを作るところまで

--- src/pages/TopPage.tsx
+++ src/pages/TopPage.tsx
@@ -1,13 +1,66 @@
 import * as React from "react";
+import SwipeableViews from "react-swipeable-views";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import { ThemeProvider } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Box from "@mui/material/Box";
+import CakeIcon from "@mui/icons-material/Cake";
+import KitchenIcon from "@mui/icons-material/Kitchen";
 import commonStyle from "../const/commonStyle";
 import ListTable from "../components/ListTable";
 import { cakeListSetting } from "../const/cakeListSetting";
+import { materialListSetting } from "../const/materialListSetting";
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  dir?: string;
+  index: number;
+  value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `full-width-tab-${index}`,
+    'aria-controls': `full-width-tabpanel-${index}`,
+  };
+}
 
 export default function TopPage() {
+  const theme = useTheme();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  const handleChangeIndex = (index: number) => {
+    setValue(index);
+  };
+
   return (
     <ThemeProvider theme={commonStyle}>
       {/*AppBar*/}
@@ -18,10 +71,38 @@ export default function TopPage() {
           </Typography>
         </Toolbar>
       </AppBar>
-      <ListTable
-        tableSetting={cakeListSetting.tableSetting}
-        itemData={cakeListSetting.initialList}
-      ></ListTable>
+      {/* main contents */}
+      <AppBar position="static" color="default">
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          indicatorColor="primary"
+          textColor="primary"
+          variant="fullWidth"
+          aria-label="full width tabs example"
+      >
+          <Tab label="販売ケーキ一覧" icon={<CakeIcon />} {...a11yProps(0)} />
+          <Tab label="在庫管理" icon={<KitchenIcon />} {...a11yProps(1)} />
+        </Tabs>
+      </AppBar>
+      <SwipeableViews
+        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+        index={value}
+        onChangeIndex={handleChangeIndex}
+      >
+        <TabPanel value={value} index={0} dir={theme.direction}>
+            <ListTable
+              tableSetting={cakeListSetting.tableSetting}
+              itemData={cakeListSetting.initialList}
+            ></ListTable>
+        </TabPanel>
+        <TabPanel value={value} index={1} dir={theme.direction}>
+            <ListTable
+              tableSetting={materialListSetting.tableSetting}
+              itemData={materialListSetting.initialList}
+            ></ListTable>
+        </TabPanel>
+      </SwipeableViews>
     </ThemeProvider>
   );
 }
